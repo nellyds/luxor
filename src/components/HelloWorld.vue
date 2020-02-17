3e3df2bd7d16991e093daeebf28a1116834d7b07
Nelson D'Silva <ndsilva822@gmail.com>
2020-02-17 14:50:10 -0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <img alt="Vue logo" src="@/assets/LUXORby-01.jpg" />
+   <p> yo dawg</p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+}
+
+</script>
