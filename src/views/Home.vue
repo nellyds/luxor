482ed3b4a3b88b4cd4b8a94e8dd40947855f6382
Nelson D'Silva <ndsilva822@gmail.com>
2020-02-17 15:13:42 -0800

    added important update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/zPBb2W9GQAg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
+  
 </template>
 
 <script>
